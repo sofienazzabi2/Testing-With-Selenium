9d79db8601996d0998580a5a88ed0f5cd7712275
sofienazzabi2 <sofien.azzabi23@gmail.com>
2023-09-02 09:37:35 +0100

    new features

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4a/2047556ebf48001e1a37b1fa72c042b0
@@ -0,0 +1,12 @@
+package Section11;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class L17_ITestListener implements ITestListener {
+	
+public void onTestFailure(ITestResult result) {
+	System.out.println("Test has failed and this message is from ITestListener interface ");
+}
+
+}
---
+++ Intro/src/Section12/L04_Login2.java
@@ -0,0 +1,34 @@
+package Section12;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class L04_Login2 extends L04_BasePage {
+	
+    @Test
+	public void signin() throws InterruptedException, IOException {
+    	
+    	driver = this.getDriver();
+    	driver.get(this.getUrl());
+		
+    	driver.findElement(By.cssSelector("[title] .hidden-sm-down")).click();
+		driver.findElement(By.cssSelector("section input[name='email']")).sendKeys("hahahahahahhaha@gmail.com");
+		driver.findElement(By.cssSelector("input[name='password']")).sendKeys("2t5GtYGJk4TNAdL");
+		driver.findElement(By.cssSelector("button#submit-login")).click();
+		System.out.println("User has logged in");
+		 Thread.sleep(2000);
+	}
+
+}
